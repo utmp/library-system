ac4e808a9735766831d29f27a7163346c7d3c927
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-05-23 14:35:55 +0300

    updated contact info & location

--- php/admin_forgot_password.php
+++ php/admin_forgot_password.php
@@ -89,14 +89,14 @@
         <div class="footer-row">
             <div class="footer-left">
                 <h1>Opening Hours</h1>
-                <p><i class="far fa-clock"></i>Monday to Friday - 9am to 9pm</p>
-                <p><i class="far fa-clock"></i>Saturday to Sunday - 8am to 11pm</p>
+                24/7 open
             </div>
             <div class="footer-right">
                 <h1>Get In Touch</h1>
                 <p>#30 abc Colony, xyz City IN<i class="fas fa-map-marker-alt"></i></p>
-                <p>example@website.com<i class="fas fa-paper-plane"></i></p>
-                <p>+8801515637957<i class="fas fa-phone-alt"></i></p>
+                                <a href="https://kutuphane.karabuk.edu.tr/index.aspx" style="color:black">website<i class="fas fa-paper-plane"></i></a>
+
+                <p>444 0 478<i class="fas fa-phone-alt"></i></p>
             </div>
         </div>
         <div class="social-links">
